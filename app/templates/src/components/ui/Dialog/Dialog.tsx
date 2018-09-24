25507a0b5432952f3182a9faeee1812a75b5f912
Falcon <gera@vervesearch.com>
2018-09-24 14:43:54 +0100

    New Dialog with props to set width and height

--- app/templates/src/components/ui/Dialog/Dialog.tsx
+++ app/templates/src/components/ui/Dialog/Dialog.tsx
@@ -2,8 +2,47 @@ import * as React from 'react';
 import * as Promise from 'bluebird';
 import VerticalAligner from '../VerticalAligner/VerticalAligner';
 export interface DialogProps {
+    /**
+     * Function to be called when dialog is closed
+     */
     close?: () => void;
-    className?: string;
+
+    /**
+     * Component parent element classname
+     */
+    className?: string; 
+
+    /**
+     * Position of the dialog box
+     */
+    position?:"top"|"bottom"|"left"|"right"|"middle";
+
+    /**
+     * Sets the padding between the dialog box and the dialog content
+     */
+    spacing?:"small"|"medium"|"large"; 
+
+    /**
+     * Height of the dialog box
+     */
+    wrapHeight?:"default"|"full-height"|"small-height"|"medium-height"|"large-height"; 
+
+    /**
+     * Width of the dialog box
+     */
+    wrapWidth?:"default"|"full-width"|"small-width"|"medium-width"|"large-width";
+
+    /**
+     * Backdrop background color
+     * Use only in stories
+     */
+    backdropColor?:string; 
+
+    /**
+     * Dialog box background color
+     * Use only in stories
+     */
+    dialogColor?:string; 
 }
 
 export interface DialogState {
@@ -17,14 +56,25 @@ export class Dialog extends React.Component<DialogProps, DialogState>{
         super(props);
     }
 
-    close = (ev?:React.SyntheticEvent<any>) => {
+    private static defaultProps: Partial<DialogProps> = {
+        className: "", 
+        position:"middle", 
+        close:()=>{}, 
+        spacing:"medium", 
+        wrapHeight:"default",
+        wrapWidth:"default", 
+        backdropColor:null, 
+        dialogColor:null
+    }
+
+    close = (ev?: React.SyntheticEvent<any>) => {
         return new Promise((res, rej) => {
-            this.animteExit(); 
+            this.animteExit();
 
             setTimeout(() => {
-                if(ev){
+                if (ev) {
                     rej(new Error("dialog rejected"))
-                }else{
+                } else {
                     res(true);
                 }
 
@@ -32,7 +82,7 @@ export class Dialog extends React.Component<DialogProps, DialogState>{
         })
     }
 
-    animteExit = ()=>{
+    animteExit = () => {
         this.backdropEl.classList.remove("animated", "fadeIn");
         this.contentWrapperEl.classList.remove("animated", "fadeInUp");
 
@@ -41,26 +91,35 @@ export class Dialog extends React.Component<DialogProps, DialogState>{
     }
 
     render() {
-        let clz = this.props.className || '';
+        let clz = this.props.className;
+        let pos = "dialog--" + this.props.position; 
+        let spacing = "dialog--spacing-" + this.props.spacing; 
+        let wrapHeight = "dialog--" + this.props.wrapHeight; 
+        let wrapWidth = "dialog--" + this.props.wrapWidth; 
         return (
-            <div className={"dialog " + clz}>
+            <div className={`dialog ${clz} ${pos} ${spacing} ${wrapHeight} ${wrapWidth}`}>
                 <div className="dialog__backdrop animated fadeIn"
                     onClick={this.props.close}
                     id="DialogBackdrop"
+                    style={{
+                        backgroundColor:this.props.backdropColor
+                    }}
                     ref={e => this.backdropEl = e}></div>
-                <VerticalAligner>
-                    <div className="dialog__wrapper animated fadeInUp"
-                        ref={e => this.contentWrapperEl = e}>
-                        <div className="dialog__close-button" 
-                            id="DialogCloseButton"
-                            onClick={this.props.close}>
-                            <i className="icon-close"></i>
-                        </div>
-                        <main className="dialog__content">
-                            {this.props.children}
-                        </main>
+
+                <div className="dialog__wrapper animated fadeInUp"
+                    style={{
+                        backgroundColor:this.props.dialogColor
+                    }}
+                    ref={e => this.contentWrapperEl = e}>
+                    <div className="dialog__close-button"
+                        id="DialogCloseButton"
+                        onClick={this.props.close}>
+                        <i className="icon-close"></i>
                     </div>
-                </VerticalAligner>
+                    <main className="dialog__content">
+                        {this.props.children}
+                    </main>
+                </div>
             </div>
         );
     }
--- app/templates/src/components/ui/Dialog/Utils.tsx
+++ app/templates/src/components/ui/Dialog/Utils.tsx
@@ -4,50 +4,136 @@ import { DP } from '../../../constants';
 import { Translation } from '../../../models/models';
 import { EmbedCode } from '../EmbedCode/EmbedCode';
 import { IS_SAFARI, IS_MOBILE } from '../../../config';
+import { Form } from '../../forms/Form/Form';
+import { DialogNormalLayout } from './Dialog';
+import { FormElement } from '../../forms/Form/FormModels';
+import { TEST_FORM } from '../../forms/Form/config';
 
-export function SHOW_EMBED_DIALOG(locale: Translation) {
-    DP.show((res, rej) => {
+export const DEFAULT_DIALOG = (content: any) => {
+
+    return DP.show((res, rej) => {
         return (
-            // <VerticalAligner>
-                <div className="embed-container container">
-    
-                    <div className="row">
-                        <div className="col s12">
-                            <h1 className="dialog__header center">{locale.embedCopyTitle}</h1>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col s12">
-                            <EmbedCode
-                                locale={locale}
-                                height={600} />
-                        </div>
-                    </div>
-    
-                </div>
-            // </VerticalAligner>
+            <DialogNormalLayout>
+                <main>
+                    {content}
+                </main>
+            </DialogNormalLayout>
         )
-    }, { className: "dialog--normal" })
+    })
+
 }
 
-export function SHOW_INFO_DIALOG(locale: Translation) {
-    let infoHtml = { __html: locale.informationCopy }
-    DP.show((res, rej) => {
+export const SHOW_DIALOG = () => {
+    return DP.show((res, rej) => {
         return (
-            // <VerticalAligner>
-                <div className="info-container container">
-    
+            <DialogNormalLayout>
+                <header className="dialog__header">
+                    <h1>this is the title</h1>
+                </header>
+                <main>
                     <div className="row">
                         <div className="col s12">
-                            <h1 className="center dialog__header">{locale.infoTitle}</h1>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad provident tempora esse unde voluptate, minus a iure porro omnis tempore eos veritatis nostrum aperiam quasi quidem possimus aliquid officia optio.</p>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="center col s12 m8 offset-m2" dangerouslySetInnerHTML={infoHtml}></div>
-                    </div>
-                </div>
-            // </VerticalAligner>
+
+                    <Form
+                        elements={TEST_FORM}
+                        onSubmit={(e) => {
+                            // console.log(e)
+                        }}
+                        onCancel={() => {
+                            // console.log("canceled")
+                        }}
+                    >
+
+                        <div className="">
+                            <input type="submit" value="Submit" className="btn" />
+                        </div>
+                    </Form>
+                </main>
+            </DialogNormalLayout>
         )
-    }, { className: "dialog--normal" })
+    }, { className: "dialog--medium" })
 }
 
+
+export const SHOW_FLAG_FORM_DIALOG = (formContents:FormElement[]) => {
+    let formEl:Form; 
+    return DP.show((res, rej) => {
+        return (
+            <DialogNormalLayout>
+                <header className="dialog__header">
+                </header>
+                <main>
+                    <Form
+                        ref={(e)=> {
+                            formEl = e;
+                        }}
+                        elements={formContents}
+                        onSubmit={(e) => {
+                            console.log("form submited")
+                            DP.hide();
+                            res(e);
+                        }}
+                        onCancel={() => {
+                            DP.hide();
+                            rej(new Error("Form Canceled"));
+                        }}
+                    >
+
+                        {/* <div className="">
+                            <Button
+                                className="button--full-width"
+                                onClick={() => {formEl.submit}}
+                            >
+                                Select
+                            </Button>
+                        </div> */}
+                    </Form>
+                </main>
+            </DialogNormalLayout>
+        )
+    }, { className: "dialog--large" })
+}
+
+
+export const SHOW_API_FORM_DIALOG = (item:any) => {
+    let formEl: Form;
+
+    return DP.show((res, rej) => {
+        return (
+            <DialogNormalLayout>
+                <header className="row dialog__header">
+                    <div className="col s12">
+                        <h2>{item.dialogTitle}</h2>
+                    </div>
+                </header>
+                <main className="link-api-input__dialog-content">
+                    <div className="row">
+                        <div className="link-api-input__description col s12"
+                            dangerouslySetInnerHTML={{ __html: item.dialogDescriptionHTML }}>
+                        </div>
+                    </div>
+                    <Form
+                        ref={e => formEl = e}
+                        elements={item.formElements}
+                        onSubmit={(e) => {
+                            DP.hide();
+                            res(e);
+                        }}>
+                        <div className="col s12">
+                            <Button
+                                className="button--full-width"
+                                onClick={() => {formEl.submit}}
+                            >
+                                Connect
+                            </Button>
+                        </div>
+                    </Form>
+                </main>
+            </DialogNormalLayout>
+        )
+    }, { className: "dialog--large" })
+
+}
