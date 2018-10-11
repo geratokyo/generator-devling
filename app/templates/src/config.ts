5c598150828e44f5cf88378f8e678f39ed51ec13
Falcon <gera@vervesearch.com>
2018-10-11 19:12:42 +0100

    API Key removed

--- app/templates/src/config.ts
+++ app/templates/src/config.ts
@@ -4,17 +4,6 @@
  */
 export const ENV = "dev";
 
-/**
- * Firebase keys and api configs
- */
-export const FIREBASE_CONF = {
-    apiKey: "AIzaSyD1853UjdHC6A9E9Ykfyvd936HZpGPIK6M",
-    authDomain: "treasured-discoveries.firebaseapp.com",
-    databaseURL: "https://treasured-discoveries.firebaseio.com",
-    projectId: "treasured-discoveries",
-    storageBucket: "",
-    messagingSenderId: "183572056148"
-}
 
 declare var process: {
     env: {
--- app/templates/src/services/Firebase.ts
+++ app/templates/src/services/Firebase.ts
@@ -5,12 +5,12 @@ declare var firebase;
  * Firebase keys and api configs
  */
 export const FIREBASE_CONF = {
-    apiKey: "AIzaSyD1853UjdHC6A9E9Ykfyvd936HZpGPIK6M",
-    authDomain: "treasured-discoveries.firebaseapp.com",
-    databaseURL: "https://treasured-discoveries.firebaseio.com",
-    projectId: "treasured-discoveries",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "183572056148"
+    messagingSenderId: ""
 }
 
 export default !firebase.apps.length ? firebase.initializeApp(FIREBASE_CONF) : firebase.app();
